7e9cda73a4379bfbc0850c01a4a1ae50986d2239
Andreas Cedervall <cederv@kth.se>
2019-09-13 09:16:40 +0200

    merged

--- src/view/overviewView.js
+++ src/view/overviewView.js
@@ -12,41 +12,28 @@ class OverviewView {
   };
 
   render() {
-    this.model.addDishToMenu(this.model.getDish());
+    this.model.addDishToMenu(this.model.getDish(592479));
+    this.model.addDishToMenu(this.model.getDish(547775));
+    this.model.addDishToMenu(this.model.getDish(818941));
+
     var content = `
-      <div id="loader" class="spinner-border" role="status">
-          <span class="sr-only">Loading...</span>
-        </div>
-      <div id="dishSearchViewWrapper">
-        
-        <div id="dishSearchBody">
-          <div id="dishSearchHeader">
-            <p class="text-center p-max-width" style="font-size: 20px;padding: 10px; padding-top: 20px;">
-              My dinner:
-              <span class="value-num-guests">${this.model.getNumberOfGuests()}</span> people
-            </p>
-            
-            <a id="goBackBtn" class="button" onclick="location.href='../screens/searchScreen.html';">
-              Go back and edit dinner
-            </a>
-          </div>
-          <div id="dishItems"></div>
-          <div><b>Total: <span class="value-total-price"></span> SEK</b></div>
-          <a id="toPrintBtn" class="button" onclick="location.href='../screens/printoutScreen.html';">
-              Print Full Recipe
-          </a>
-          
+      <div>
+        <div id='pageHeader'></div>
+        <div id='pageBody'>
+          <span id='dishItems'></span>  
         </div>
       </div>`;
+
     this.container.innerHTML = content;
     console.log('gettfullmenu', this.model.getFullMenu());
     this.model.getFullMenu().forEach(dish => {
+      console.log('dish', dish);
       document.getElementById('dishItems').innerHTML += `
           <div class="dish">
             <img class="dishImage image border" src="${this.model.getDishImageURLFromString(
               dish.image
             )}"/>
-            <p class="dishText value-main-course-name">${cutOverflowingText(dish.title)}</p>
+            <p class="dishText value-main-course-name">${this.cutOverflowingText(dish.title)}</p>
             <p class="dishText">${dish.pricePerServing} SEK</p>
           </div>`;
     });
