93c1af65069953cfdf86f65e843242e7c9d56207
rintala <jrintala@kth.se>
2019-09-13 18:01:07 +0200

    Further cleanup of code

--- src/controller/generalStateController.js
+++ src/controller/generalStateController.js
@@ -1,25 +1,14 @@
 class GeneralStateController {
   constructor(view, model) {
     this.views = {};
-
-    // TODO lab 3
   }
 
-  update(payload) {
-    // TODO Lab 3
-  }
-
-  renderView(viewToRender) {
-    viewToRender.render();
-    // TODO lab 3
-  }
-
-  // TODO Lab 3
   setView(viewName, view) {
     this.views[viewName] = view;
   }
 
   displayView(viewName) {
-    this.renderView(this.views[viewName]);
+    const viewToRender = this.views[viewName];
+    viewToRender.render();
   }
 }
