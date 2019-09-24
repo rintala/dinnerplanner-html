e3d84b46de63d2c91b0f019df0a9d0ca7f640c38
rintala <jrintala@kth.se>
2019-09-24 17:05:25 +0200

    Pass some more tests

--- src/controller/sidebarController.js
+++ src/controller/sidebarController.js
@@ -1,38 +1,20 @@
 class SideBarController {
   constructor(view, model, generalStateController) {
-    console.log("Sidebar is being created");
     this.view = view;
     this.model = model;
     this.generalStateController = generalStateController;
-
     this.renderView = this.renderView.bind(this);
-    // TODO lab 3
-
-    //  console.log("sidebar view");
-    //  console.log("gsc", this.generalStateController);
-    const self = this;
-    //  console.log("self", self);
-    /* this.renderView(self); */
   }
 
   hideView() {
     this.view.container.innerHTML = "";
   }
 
-  async renderView() {
-    console.log("SELF", document);
-    await this.view.render();
-    console.log("thisc onfirm", this.view.confirmButton);
-    console.log("this model", this.model);
-    /* const confirmButton = this.view.container.querySelector("#confirmBtn"); */
-    //  console.log("this.view.confirmButton", this.view.confirmButton);
+  renderView() {
+    this.view.render();
     this.view.confirmButton.addEventListener("click", () => {
-      console.log("click to confirm");
       window.location = "#overview";
-      /*  this.generalStateController.hideAllViews();
-      this.generalStateController.displayView("overview"); */
     });
-    //  console.log("inputnubmerchanger", this.view.inputNumberChanger);
     this.view.inputNumberChanger.addEventListener("input", e => {
       if (!isNaN(parseInt(e.data))) {
         this.model.setNumberOfGuests(parseInt(e.data));
